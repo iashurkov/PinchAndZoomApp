6e14144265c1a8c3ebf89e245d998c4b0da1e846
iashurkov <iashurkov@uma.tech>
2022-11-04 13:22:10 +0300

    Add double tap gesture.

--- PinchAndZoomApp_SwiftUI/Screen/ContentView.swift
+++ PinchAndZoomApp_SwiftUI/Screen/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Property
+    
+    @State private var isAnimating = false
+    @State private var imageScale: CGFloat = 1
+    
+    // MARK: Body
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            ZStack {
+                
+                // MARK: Page Image
+                
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(self.isAnimating ? 1 : 0)
+                    .animation(.linear(duration: 1), value: self.isAnimating)
+                    .scaleEffect(self.imageScale)
+                // MARK: #1. Gesture Tap
+                    .onTapGesture(count: 2, perform: {
+                        if self.imageScale == 1 {
+                            withAnimation(.spring()) {
+                                self.imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                self.imageScale = 1
+                            }
+                        }
+                    })
+                
+            } //: ZStack
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                self.isAnimating = true
+            })
+        } //: NavigationView
+        .navigationViewStyle(.stack)
     }
 }
 
