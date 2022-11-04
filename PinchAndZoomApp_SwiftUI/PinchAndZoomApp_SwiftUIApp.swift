a5d6d8f95fe5292aefd2e84f5b8d9fb5b2ad2a8f
iashurkov <iashurkov@uma.tech>
2022-11-04 12:47:20 +0300

    Initial Commit

---
+++ PinchAndZoomApp_SwiftUI/PinchAndZoomApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAndZoomApp_SwiftUIApp.swift
+//  PinchAndZoomApp_SwiftUI
+//
+//  Created by Igor Ashurkov on 04.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchAndZoomApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
